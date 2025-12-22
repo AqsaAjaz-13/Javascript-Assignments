623d088457e6269432be7058190e23b0932b77fb
AqsaAjaz-13 <aqsaajaz904@gmail.com>
2025-12-22 15:04:13 +0500

    complete

---
+++ Assignment#1/App.js
@@ -0,0 +1,24 @@
+// Ques#1
+alert("welcome to our website");
+
+// Ques#2
+alert("Error! Please Enter a valid password");
+
+// Ques#3
+alert("welcome to JS land... \n Happy coding!");
+
+// Ques#4a
+
+alert("welcome to JS land...");
+
+// Ques#4b
+
+alert("Happy coding! \n prevent this page for creating additional dialogs.");
+
+
+// Ques#5
+
+alert("Hello... I can learn JS through my website browser's console");
+
+
+
---
+++ Assignment#2/App.js
@@ -0,0 +1,48 @@
+// Ques#1
+
+var username = "Aqsa"
+
+// Ques#2
+
+var myName = "Aqsa"
+console.log(myName); 
+
+// Ques#3
+var message = "Hello World"
+
+alert("Hello world");
+
+
+// Ques#4a
+var studentName = "hania Fahim"
+
+alert(studentName);
+
+// Ques#4b
+var studentAge = "20 years"
+
+alert(studentAge);
+
+// Ques#4c
+var studentField = "Mobile application developer"
+
+alert(studentField);
+
+// Ques#5
+var content = "PIZZA\nPIZZ\nPIZ\nPI\np"
+alert(content);
+
+// Ques#6
+var email = " My Email Address is aqsaajaz5@gmail.com"
+alert(email);
+
+// Ques#7
+var book = " A smarter way to learn Javascript"
+alert(book);
+
+
+
+
+
+
+
