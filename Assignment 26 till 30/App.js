0d395012d06db0e09eb8a571884c00aa6089852b
AqsaAjaz-13 <aqsaajaz904@gmail.com>
2026-01-28 15:06:14 +0500

    chp 31 to 34

---
+++ Assignment 26 till 30/App.js
@@ -0,0 +1,20 @@
+//Ques # 1
+var num = +prompt(" Enter a positive integer");
+document.write("Number :" + num + "<br>" );
+document.write("round off :" + Math.round(num)  + "<br>");
+document.write("floor value :" + Math.floor(num) +"<br>" );
+document.write("ceil value :" + Math.ceil(num) +"<br>" + "<br>");
+
+//ques #2
+var value = +prompt(" Enter a negative floating point");
+document.write("Number :" + value + "<br>" );
+document.write("round off :" + Math.round(value)  + "<br>");
+document.write("floor value :" + Math.floor(value) +"<br>" );
+document.write("ceil value :" + Math.ceil(value) +"<br>" );
+
+//Ques #3 
+var number = prompt("Enter any number positive/negative");
+var absValue = Math.abs(number);
+document.write( "the absolute value of -66 :" + absValue);
+
+//ques # 4
