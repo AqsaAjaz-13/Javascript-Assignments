ebe8a4b0205b2ca879c95a6c544fdf82bbe05574
AqsaAjaz-13 <aqsaajaz904@gmail.com>
2026-03-04 09:09:02 +0500

    complete

---
+++ Assignment #43 till 48/App.js
@@ -0,0 +1,54 @@
+// Ques 1
+var alertLink = document.getElementById("alertLink");
+
+alertLink.onclick = function () {
+    alert("You clicked the link!");
+};
+
+//ques 2
+var img1 = document.getElementById("img1");
+var img2 = document.getElementById("img2");
+var img3 = document.getElementById("img3");
+var img4 = document.getElementById("img4");
+
+// Buy Buttons
+var buy1 = document.getElementById("buy1");
+var buy2 = document.getElementById("buy2");
+var buy3 = document.getElementById("buy3");
+var buy4 = document.getElementById("buy4");
+
+
+// Image Click Events
+img1.onclick = function () {
+    alert("You clicked Image 1");
+};
+
+img2.onclick = function () {
+    alert("You clicked Image 2");
+};
+
+img3.onclick = function () {
+    alert("You clicked Image 3");
+};
+
+img4.onclick = function () {
+    alert("You clicked Image 4");
+};
+
+
+// Buy Now Button Events
+buy1.onclick = function () {
+    alert("Thanks for purchasing");
+};
+
+buy2.onclick = function () {
+    alert("Thanks for purchasing");
+};
+
+buy3.onclick = function () {
+    alert("Thanks for purchasing");
+};
+
+buy4.onclick = function () {
+    alert("Thanks for purchasing");
+};
